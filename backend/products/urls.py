e5adbdaf0690ac868ac28b82333b58b613e4edbc
Umesh <umeshtamang129@gmail.com>
2025-12-25 10:36:05 +0545

    ViewSets & Routers

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .import views
+from django.urls import path, include
+from .views import ProductView
+from rest_framework import routers
+
+route = routers.DefaultRouter()
+route.register("", ProductView, basename='productview')
 
 
 urlpatterns = [
-    
+    path('api/', include(route.urls))
 ]
